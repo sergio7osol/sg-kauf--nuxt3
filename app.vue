f0d88aadcd1149435b0a816e873ab3b367d9780c
Sergey Osokin <sergey8osokin@gmail.com>
2024-11-24 22:55:09 +0100

    - added main styles in assets, made variables global; - added basic layouts and a page; - set up initial App compoment and displayed the page in there;

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup>
+  useHead({
+    titleTemplate: (titleChunk) => titleChunk ? `${titleChunk} - Money tracking` : 'Money tracking App',
+    meta: [
+      { name: 'description', content: 'Money tracking and planning App' },
+    ],
+  });
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    "@/assets/styles/variables.css", 
+  ],
+  modules: [
+    // ['@pinia/nuxt', {
+    //   autoImports: ['defineStore', 'acceptHMRUpdate']
+    // }]
+  ],
+  routeRules: {
+    // "/spa": {ssr: false},
+    // "/static": {static: true},
+    // "/swr": {swr: false}
+  },
+  // alias: {
+  //   pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs'
+  // }
 })
+ 
