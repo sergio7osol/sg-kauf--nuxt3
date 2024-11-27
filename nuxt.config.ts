e484cc8852a62cee418d75baa2aeaa8d78979cf6
Sergey Osokin <sergey8osokin@gmail.com>
2024-11-27 22:35:05 +0100

    - added bootstrap, prime vue, loadash, axios; - added generic bg-pic;

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: [
-    "@/assets/styles/variables.css", 
+    "bootstrap/dist/css/bootstrap.min.css",
+    // "primevue/resources/themes/saga-blue/theme.css",
+    // "primevue/resources/primevue.css",
+    // 'primeicons/primeicons.css',
+    "@/assets/styles/variables.css",
+    "@/assets/styles/main.css",
   ],
-  modules: [
-    // ['@pinia/nuxt', {
-    //   autoImports: ['defineStore', 'acceptHMRUpdate']
-    // }]
-  ],
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    options: {
+        ripple: true,
+        inputVariant: 'filled',
+        theme: {
+            preset: Aura,
+            options: {
+                prefix: 'prime-',
+                darkModeSelector: 'system',
+                cssLayer: false
+            }
+        }
+    }
+},
   routeRules: {
     // "/spa": {ssr: false},
     // "/static": {static: true},
-    // "/swr": {swr: false}
+    // "/swr": {swr: false} 
   },
   // alias: {
   //   pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs'
   // }
-})
- 
+});
